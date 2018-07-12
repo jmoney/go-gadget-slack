4c1ea57f13d440072c95d8471b01bee049ca33fa
J$ <jon.monette@gmail.com>
2018-07-12 18:24:06 -0500

    remove logging and return error on error

--- slack.go
+++ slack.go
@@ -2,32 +2,9 @@ package slack
 
 import (
 	"encoding/json"
-	"io/ioutil"
-	"log"
 	"net/http"
-	"os"
 	"strings"
 )
-
-var (
-	infoLogger    *log.Logger
-	warningLogger *log.Logger
-	errorLogger   *log.Logger
-)
-
-func init() {
-	infoLogger = log.New(os.Stdout,
-		"[INFO]: ",
-		log.Ldate|log.Ltime|log.Llongfile)
-
-	warningLogger = log.New(os.Stdout,
-		"[WARNING]: ",
-		log.Ldate|log.Ltime|log.Llongfile)
-
-	errorLogger = log.New(os.Stderr,
-		"[ERROR]: ",
-		log.Ldate|log.Ltime|log.Llongfile)
-}
 
 // Send function to post a slack payload to incoming webhook
 func (slackClient *Client) Send(payload Payload) (string, error) {
@@ -36,12 +13,8 @@ func (slackClient *Client) Send(payload Payload) (string, error) {
 	req, err := http.NewRequest("POST", slackClient.webhook, strings.NewReader(rawRequestJSON))
 	resp, err := slackClient.http.Do(req)
 	if err != nil {
-		warningLogger.Println(err)
 		return "", err
 	}
 	defer resp.Body.Close()
-
-	rawResponseJSON, err := ioutil.ReadAll(resp.Body)
-	infoLogger.Printf("status_code=%s slack_response_raw=%s", resp.Status, rawResponseJSON)
 	return resp.Status, nil
 }
